52843b6ad0e159074b3989219a6e7ec1094013a3
Sameer Ansari <elucidation@gmail.com>
2011-12-14 22:43:00 -0500

    added mexglx files and new noise filter (uncertainty based on distance also)

---
+++ noiseFilterDist.m
@@ -0,0 +1,31 @@
+function [filterV sigmaV] = noiseFilterDist()
+% Returns 2 x numPoints noise values
+% where the noise variance is dependent on the values of viewsCount
+% Made it slightly exponential
+% OUTPUT
+%   noise filter function using these values (func of viewCount
+%   sigma function for this noise filter (func of viewCount)
+
+%%%%%%%%
+% NOISE FILTER
+%%%%%%%%
+% Gaussian noise, mean = 0, variance = 1
+NOISE_MEAN = 0;
+% View of 1 = variance of MAX_VARIANCE
+MAX_VARIANCE = 2; % Worst possible error in x,y values (not distance, worst dist is sqrt(2*MAX_VARIANCE^2)
+MIN_VARIANCE = 0.0001; % Best possible error in x,y values
+POWR = 1.4; % Exponential factor (1+, higher equals faster noise loss)
+PERFECT_VIEW = 25; % At how many views till no noise
+
+PERFECT_VIEW = nthroot(PERFECT_VIEW,POWR); % This accounts for powers
+
+%variance = MAX_VARIANCE*max(MIN_V)ARIANCE,PERFECT_VIEW-viewsCount.^POWR+1)/PERFECT_VIEW; % MAX_VARIANCE -> 0 sigma = variance;
+
+sigmaV = @(viewsCount,distances,k) k*distances.*MAX_VARIANCE.*...
+    max(MIN_VARIANCE,PERFECT_VIEW-viewsCount.^POWR+1)./PERFECT_VIEW;
+
+% views must be vector of length numPoints
+filterV = @(viewsCount,distances,k) NOISE_MEAN + ...
+    sigmaV([viewsCount;viewsCount],[distances;distances],k).*randn(2,size(viewsCount,2));
+
+end
